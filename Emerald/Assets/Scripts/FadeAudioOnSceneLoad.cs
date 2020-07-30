ec9748d6d6a736a24d262978665417d3a04f17ac
lodos3 <djgozzett@gmail.com>
2020-07-30 21:31:25 +0100

    LoginScene
    - Animations changed to 4 second and tweaked
    - Door Burst FX temp added
    - Sound scene transitions added
    - Mountain climbed
    - Mole hill stomped
    - Adjusted mesh sizes (Right door was skinnier and offset)
    - Lighting adjusted

---
+++ Emerald/Assets/Scripts/FadeAudioOnSceneLoad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+
+public class FadeAudioOnSceneLoad : MonoBehaviour
+{
+    public bool fade = false;
+    public AudioSource m_AudioSource; // Probably be better to fade in "Music" audio mixer only
+    public AudioMixer m_AudioMixer;   // by using this instead m_AudioMixer.SetFloat("musicVol", value)
+    [Range(0f, 10f)]
+    public float musicFadeSpeed;
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {       
+        fade = true;
+    }
+
+    void Update()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        if (fade == true)
+        {
+            m_AudioSource.volume = Mathf.Lerp(m_AudioSource.volume, 0, musicFadeSpeed * Time.deltaTime);
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }    
+}
